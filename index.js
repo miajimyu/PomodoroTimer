6c4bf8f87490a4e58edf90dc4c441201c2207b2b
miajimyu <38499336+miajimyu@users.noreply.github.com>
2019-01-24 23:37:44 +0900

    delete reload

--- index.js
+++ index.js
@@ -111,7 +111,6 @@ if (process.env.NODE_ENV !== 'production') {
   menuTemplate.push({
     label: 'View',
     submenu: [
-      { role: 'reload' },
       {
         label: 'Toggle Developer Tools',
         accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',

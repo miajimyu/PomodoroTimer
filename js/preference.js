91c8772882938a1cbfc1ce6af544ccdd80c52500
miajimyu <38499336+miajimyu@users.noreply.github.com>
2019-01-25 22:40:55 +0900

    Add Preference Save Default.

--- js/preference.js
+++ js/preference.js
@@ -10,6 +10,7 @@ const TARGETINTERVAL = 6;
 const ALWAYSONTOP = true;
 const AUTOSTARTTIMER = false;
 
+let form1 = document.form1;
 let workInterval = document.form1.workInterval;
 let shortBreak = document.form1.shortBreak;
 let longBreak = document.form1.longBreak;
@@ -17,6 +18,7 @@ let longBreakAfter = document.form1.longBreakAfter;
 let targetInterval = document.form1.targetInterval;
 let alwaysOnTop = document.form1.alwaysOnTop;
 let autoStartTimer = document.form1.autoStartTimer;
+let saveButton = document.getElementById('save-button');
 
 function setConfigNumber(name, number) {
   config.set(name, Number(number));
@@ -26,30 +28,44 @@ function setConfigBoolean(name, bool) {
   config.set(name, Boolean(bool));
 }
 
+function changeSaveButtonEnable(params) {
+  saveButton.style.backgroundColor = 'black';
+  saveButton.style.color = 'white';
+}
+
+form1.addEventListener('change', () => {
+  saveButton.style.backgroundColor = 'black';
+  saveButton.style.color = 'white';
+})
+
 workInterval.addEventListener('change', () => {
-  setConfigNumber('workInterval', workInterval.value);
+  // setConfigNumber('workInterval', workInterval.value);
 });
 shortBreak.addEventListener('change', () => {
-  setConfigNumber(shortBreak.value);
+  // setConfigNumber(shortBreak.value);
 });
 longBreak.addEventListener('change', () => {
-  setConfigNumber('longBreak', longBreak.value);
+  // setConfigNumber('longBreak', longBreak.value);
 });
 longBreakAfter.addEventListener('change', () => {
-  setConfigNumber('longBreakAfter', longBreakAfter.value);
+  // setConfigNumber('longBreakAfter', longBreakAfter.value);
 });
 targetInterval.addEventListener('change', () => {
-  setConfigNumber('targetInterval', targetInterval.value);
+  // setConfigNumber('targetInterval', targetInterval.value);
 });
 alwaysOnTop.addEventListener('change', () => {
-  setConfigBoolean('alwaysOnTop', alwaysOnTop.value);
+  // setConfigBoolean('alwaysOnTop', alwaysOnTop.value);
 });
 autoStartTimer.addEventListener('change', () => {
-  setConfigBoolean('autoStartTimer', autoStartTimer.value);
+  // setConfigBoolean('autoStartTimer', autoStartTimer.value);
 });
 
 function OnDefaultButtonClick(){
   setDefaultPreferenceParameter();
+  setConfigAll();
+}
+
+function OnSaveButtonClick() {
   setConfigAll();
 }
 
